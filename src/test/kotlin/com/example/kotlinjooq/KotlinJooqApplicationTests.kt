95fe750a20b9a46236625d991cf68a3f910116e8
heosangbeom <luv_3_u@naver.com>
2025-03-15 17:45:09 +0900

    first commit

---
+++ src/test/kotlin/com/example/kotlinjooq/KotlinJooqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinjooq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJooqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
